59efc0b6a78e3194226ce26544cea320b206fab1
nizarrahmoun <nizarrahmoun7@gmail.com>
2025-04-20 19:07:49 +0100

    setup environment

---
+++ src/main/java/com/example/systeme_suivi_ticket/SystemeSuiviTicketApplication.java
@@ -0,0 +1,13 @@
+package com.example.systeme_suivi_ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemeSuiviTicketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemeSuiviTicketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/systeme_suivi_ticket/SystemeSuiviTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.systeme_suivi_ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemeSuiviTicketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
